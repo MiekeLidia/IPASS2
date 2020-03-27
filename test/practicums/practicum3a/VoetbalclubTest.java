236222b168da9a3db72161096b00c9b103842a1d
Lidia <60886622+MiekeLidia@users.noreply.github.com>
2020-03-27 13:44:04 +0100

    practicum 3a, de test

--- test/practicums/practicum3a/VoetbalclubTest.java
+++ test/practicums/practicum3a/VoetbalclubTest.java
@@ -9,22 +9,23 @@ class VoetbalclubTest {
 
 
     @Test
+    //Eis: als de clubnaam null of leeg ("") is, moet de naam "FC" gebruikt worden.
     void standaardFC(){
-        Voetbalclub test = new Voetbalclub("test");
-        assertSame("FC", test.);
+        Voetbalclub test = new Voetbalclub("");
+        System.out.println(test);
     }
-
     @Test
+    //Eis: het verwerken van de resultaten ‘w’, ‘g’ en ‘v’ levert respectievelijk 3, 1 en 0 punten op.
+        //Foutieve invoer mag het puntensaldo en totaalaantal gespeelde wedstrijden niet beïnvloeden!
+    //Eis: het verwerken van de resultaten ‘w’, ‘g’ en ‘v’ levert respectievelijk 3, 1 en 0 punten op,
+        // en is zichtbaar in de stringrepresentatie van de toString-methode.
     void geenWGV(){
         Voetbalclub test2 = new Voetbalclub("test2");
         test2.verwerkResultaat('h');
+        test2.verwerkResultaat('g');
+        test2.verwerkResultaat('w');
+        System.out.println(test2);
     }
-
-    @Test
-    void ongeldigNietInString(){
-        Voetbalclub test3 = new Voetbalclub("test3");
-
-
-    }
+    
 
 }
